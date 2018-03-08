ffa14fcce68818d07ab6696c3bf3d2e0e87fa621
lee kisung <01074881401@gmail.com>
2018-03-08 19:09:49 +0900

    test

---
+++ spring-boot-journal8/src/main/java/com/apress/spring/SpringBootJournal8Application.java
@@ -0,0 +1,12 @@
+package com.apress.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJournal8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJournal8Application.class, args);
+	}
+}
